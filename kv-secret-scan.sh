c8919c11ef56ea05df5432f3d0e390f8e55bffa0
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-05-23 10:16:31 +0100

    Switch to linux date

--- kv-secret-scan.sh
+++ kv-secret-scan.sh
@@ -2,8 +2,8 @@
 set -e
 
 TZ=Europe/London
-TODAY=$(gdate -Idate)
-DATE_90=$(gdate --date="90 days ago" +"%Y-%m-%d")
+TODAY=$(date -Idate)
+DATE_90=$(date --date="90 days ago" +"%Y-%m-%d")
 SILENT=0
 
 NOTIFY=1
